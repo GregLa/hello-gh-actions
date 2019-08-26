6fd5f71927100b4c82a12f9964ffdde403a05ac4
Grégoire LAGOUTTE <glagoutte@DAESIGN.COM>
2019-08-26 17:04:48 +0200

    Test with crappy js

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
-import express from "express"
+// import express from "express"
 
-export class Server {
-    private app: any
-    constructor() {
-        this.app = express()
-        this.app.get("/", function(req, res) {
-            res.set("Content-Type", "text/plain")
-            res.send("Hello actions !")
-        })
+// export class Server {
+//     private app: any
+//     constructor() {
+//         this.app = express()
+//         this.app.get("/", function(req, res) {
+//             res.set("Content-Type", "text/plain")
+//             res.send("Hello actions !")
+//         })
 
-        this.app.listen(3003, () => {
-            console.log(`Server running on port "http://localhost:3003"`)
-        })
-    }
-}
+//         this.app.listen(3003, () => {
+//             console.log(`Server running on port "http://localhost:3003"`)
+//         })
+//     }
+// }
 
-const srv: Server = new Server()
+// const srv: Server = new Server()
+
+let express = require("express")
+let app = express()
+let bodyParser = require("body-parser")
+let port = 3003
+
+//parse application/json and look for raw text
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: "application/json" }))
+
+app.get("/", function(req, res) {
+    res.set("Content-Type", "text/plain")
+    res.send("Hello actions !")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port "http://localhost:3003"`)
+})
+
+module.exports = app // for testing
--- test/unit/test.js
+++ test/unit/test.js
@@ -1,13 +1,13 @@
 let chai = require("chai")
 let expect = chai.expect
 let chaiHttp = require("chai-http")
-let server = "http://localhost:3003"
+let app = require("../../dist/index")
 
 chai.use(chaiHttp)
 describe("API", function() {
     describe("GET '/'", function() {
         it("status 200", (done) => {
-            chai.request(server)
+            chai.request(app)
                 .get("/")
                 .end((err, res) => {
                     expect(res).to.have.status(200)
@@ -15,7 +15,7 @@ describe("API", function() {
                 })
         })
         it("content", (done) => {
-            chai.request(server)
+            chai.request(app)
                 .get("/")
                 .end((err, res) => {
                     expect(res.text).to.eq("Hello actions !")
