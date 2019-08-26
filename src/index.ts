de83174b6ce39aba2409d8d631a9d3568061ce6b
Grégoire LAGOUTTE <glagoutte@DAESIGN.COM>
2019-08-26 14:39:21 +0200

    Basic server up and running

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+export class Server {
+    private app: any
+    constructor() {
+        this.app = express()
+        this.app.get('/', function(req, res) {
+            res.send('Hello actions !')
+        })
+
+        this.app.listen(process.env.PORT, () => {
+            console.log('Server running on port ' + process.env.PORT)
+        })
+    }
+}
+
+const srv: Server = new Server()
